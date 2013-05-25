edfe6de9a6bfdc6c7663437f5e6644684e01ae58
Andrew Stamm <andrewstamm@me.com>
2013-05-25 08:53:08 -0500

    pagination fix for like links

--- public/js/application.js
+++ public/js/application.js
@@ -61,6 +61,9 @@ $(document).ready(function() {
       var page = $(results).filter('#pager').html();
       $('.container').html(like);
       $('#pager').html(page);
+      // Pagination Fix
+      $('.next_page').text('Next');
+      $('.previous_page').text('Previous');
     });
   });
 
