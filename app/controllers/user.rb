19b15c68937e9a573a39015947dfbedbbcf7d1a2
Andrew Stamm <andrewstamm@me.com>
2013-05-26 14:31:07 -0500

    changed new account redirect

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -32,7 +32,8 @@ post '/create_account' do
                             :password   => params[:password])
   if @new_user.save
     session[:user_id] = @new_user.id
-    redirect to "/author/#{@new_user.id}"
+    # redirect to "/author/#{@new_user.id}"
+    redirect "/"
   else
     @errors = "Full Name, a valid email and a password<br>of at least 5 characters are required"
     erb :signup
