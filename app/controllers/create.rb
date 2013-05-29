a730eae73c20b8c42afe1e30c3cb9a6c6c0c9b72
Andrew Stamm <andrewstamm@me.com>
2013-05-29 07:49:36 -0500

    fixed create haiku bug

--- app/controllers/create.rb
+++ app/controllers/create.rb
@@ -10,7 +10,7 @@ post '/create_haiku' do
   @new_haiku = Haiku.create(  :line_1   => params[:line_1],
                               :line_2   => params[:line_2],
                               :line_3   => params[:line_3],
-                              :user_id  => current_user.id )
+                              :author_id  => current_user.id )
   if @new_haiku.save
     redirect to "/haiku/#{@new_haiku.id}"
   else
