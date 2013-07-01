15fc76e306e9927e0d958c207b65968e4ecf96bd
Andrew Stamm <andrewstamm@me.com>
2013-07-01 16:18:05 -0500

    validates main haiku creation

--- app/controllers/create.rb
+++ app/controllers/create.rb
@@ -7,14 +7,14 @@ get '/create' do
 end
 
 post '/create_haiku' do
-  @new_haiku = Haiku.create(  :line_1   => params[:line_1],
+  @new_haiku = Haiku.new(  :line_1   => params[:line_1],
                               :line_2   => params[:line_2],
                               :line_3   => params[:line_3],
                               :author_id  => current_user.id )
   if @new_haiku.save
     redirect to "/haiku/#{@new_haiku.id}"
   else
-    @errors = "You must enter in all three lines."
+    @errors = "Not a valid haiku"
     erb :create
   end
 end
--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -12,6 +12,19 @@ class Haiku < ActiveRecord::Base
 
   self.per_page = 3
 
+  before_save :valid_haiku?
+
+  def valid_haiku?
+    count_syllables(self.line_1) == 5 && count_syllables(self.line_2) == 7 && count_syllables(self.line_3) == 5
+  end
+
+  def count_syllables(input)
+    counts = input.gsub(/[\-\,\.\b\s]+|\\n/, " ").split(" ").map do |word|
+      Dictionary.new.dictionary[word.upcase.to_sym]
+    end
+    counts.inject(&:+)
+  end
+
   def self.by_likes
     Haiku.all.sort!{ |a,b| a.likes.count <=> b.likes.count }.reverse!
   end
