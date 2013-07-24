f80683fb488bbc5a40b0056dfd31b4b1f1a51ebf
Andrew Stamm <andrewstamm@me.com>
2013-07-24 16:40:18 -0500

    Added to mailing list

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -6,7 +6,18 @@ class Mailer < ActionMailer::Base
     @line_3 = line_3
     @id = id
     mail(
-      :to      => "Andrew Stamm <andrewstamm@me.com>, Rod Levy <rodlevy1@gmail.com>, Milan Bhandari <milan_bhandari@hotmail.com>, Brett Friell <brett.friell@gmail.com>, Quinlan H DeMay <qdemay@yahoo.com>, Emily Patton <emilydpatton@yahoo.com>, Joe Levickas <joelevickas@gmail.com>, Erik Stanley <erikuofi@yahoo.com>, Lindsey Nordmark <behindstyle@hotmail.com>, Emily Lovig <emilylovig@gmail.com>, Erin Sanders <erinrenay@gmail.com>, Beau Sievers <beau.sievers@gmail.com>, Cameron Honsa <chonsa1@gmail.com>, Travis Saunders <macdhaibhidh@hotmail.com>, Hannah Halder <hannah.halder@gmail.com>, Kate Donahoe <donahoe@gmail.com>, Francisco Quinteros <faquinteros@gmail.com>, Amy Singleterry <singleta418@yahoo.com>, Cassie Speer <cassafrass_00@yahoo.com>, Kimberly Berls <kimberlyberls@gmail.com>, Kelly Daily <kellykdaily@gmail.com>, Neil Bhandari <neil.bhandari@gmail.com>",
+      :to      => " Andrew Stamm <andrewstamm@me.com>, Rod Levy <rodlevy1@gmail.com>,
+                    Milan Bhandari <milan_bhandari@hotmail.com>, Brett Friell <brett.friell@gmail.com>,
+                    Quinlan H DeMay <qdemay@yahoo.com>, Emily Patton <emilydpatton@yahoo.com>,
+                    Joe Levickas <joelevickas@gmail.com>, Erik Stanley <erikuofi@yahoo.com>,
+                    Lindsey Nordmark <behindstyle@hotmail.com>, Emily Lovig <emilylovig@gmail.com>,
+                    Erin Sanders <erinrenay@gmail.com>, Beau Sievers <beau.sievers@gmail.com>,
+                    Cameron Honsa <chonsa1@gmail.com>, Travis Saunders <macdhaibhidh@hotmail.com>,
+                    Hannah Halder <hannah.halder@gmail.com>, Kate Donahoe <donahoe@gmail.com>,
+                    Francisco Quinteros <faquinteros@gmail.com>, Amy Singleterry <singleta418@yahoo.com>,
+                    Cassie Speer <cassafrass_00@yahoo.com>, Kimberly Berls <kimberlyberls@gmail.com>,
+                    Kelly Daily <kellykdaily@gmail.com>, Neil Bhandari <neil.bhandari@gmail.com>,
+                    liz Abinante <eabinante@gmail.com>",
       :bcc     => "Squirrels 2013 <squirrels.2013@devbootcamp.com>",
       :from    => "andrewstamm@me.com",
       :subject => "Hump Day Haiku") do |format|
